7e7585863dfb2736d957e447908708963cb65ec2
bgonzalezzz <94342391+bgonzalezzz@users.noreply.github.com>
2022-01-24 20:48:48 +1100

    Added relevant icon from API

--- src/index.js
+++ src/index.js
@@ -54,6 +54,12 @@ function getTemp(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function showCurrentLocation(position) {
